e55e7ecdbbea84c6f852e8bdf8177377e7c3af61
olamphzy <olanrwajurasheed@yahoo.com>
2023-08-21 19:56:15 +0100

    Disney_Hotstar

---
+++ movies_data.js
@@ -0,0 +1,82 @@
+let movies = [
+    {
+        name: 'Rio 2 (2014)',
+        des: 'After moving out of Rio de Janeiro, Blue, Jewel and their children try to cope with new surroundings in the Amazon. But Blu encounters the revengeful Nigel who further complicates the situation',
+        image: 'images/slider 1.JPG'
+    },
+    {
+        name: 'Aquaman and the Lost Kingdom',
+        des: 'Aquaman forges an uneasy alliance with an unlikely ally in a bid to save Atlantis and the rest of the planet',
+        image: 'images/slider 2.JPG'
+    },
+    {
+        name: 'Brave',
+        des: 'Merida (Kelly Macdonald), the impetuous but courageous daughter of Scottish King Fergus and Queen Elinor, is a skilled archer who wants to carve out her own path in life. Now, Merida must discover the true meaning of courage and undo a beastly curse before its too late.',
+        image: 'images/slider 3.jpg'
+    },
+    {
+        name: 'Raya and the Last Dragon',
+        des: 'Raya, a warrior, sets out to track down Sisu, a dragon, who transferred all her powers into a magical gem which is now scattered all over the kingdom of Kumandra, dividing its people',
+        image: 'images/slider 4.PNG'
+    },
+    {
+        name: 'Kung Fu Panda',
+        des: 'Unlike his father, who also dreamed but never found the courage to act on it, Panda follows his calling and by "luck" get chosen to be the legendary Dragon',
+        image: 'images/slider 5.jpg'
+    },
+    {
+        name: 'Tangled (2010)',
+        des: 'Rapunzel, a naive and young girl, is locked up by her overly protective mother. Her wish to escape into the world outside finally comes true when she meets the good-hearted thief, Flynn',
+        image: 'images/slider 6.jpg'
+    },
+    {
+        name: 'Iron Man 3 (2013)',
+        des: 'Tony Stark encounters a formidable foe called the Mandarin. After failing to defeat his enemy, Tony embarks on a journey of self-discovery as he fights against the powerful Mandarin.',
+        image: 'images/slider 7.jpg'
+    },
+    {
+        name: 'Beauty and the Beast (2017)',
+        des: 'Belle embarks on a journey to save her father from a creature that has locked him in his dungeon. Eventually, she learns that the beast is an enchanted prince who has been cursed.',
+        image: 'images/slider 8.jpg'
+    },
+    {
+        name: 'Epic (2013)',
+        des: 'When Mary, a 17-year-old teenager, shrinks in size, she discovers a world inhabited by strange creatures. Soon, she finds herself reluctantly pulled into a battle between good and evil forces.',
+        image: 'images/slider 9.jpg'
+    },
+    {
+        name: 'Spider-Man: No Way Home',
+        des: "Spider-Man seeks the help of Doctor Strange to forget his exposed secret identity as Peter Parker. However, Strange's spell goes horribly wrong, leading to unwanted guests entering their universe.",
+        image: 'images/slider 10.jpg'
+    },
+    {
+        name: 'Dracular Untold (2014)',
+        des: 'Luke Evans as Vlad III "the Impaler" Drăculea / Dracula: Known as the Son of the Dragon, Vlad is a prince and war hero, who becomes a monster known ..',
+        image: 'images/slider 11.jpg'
+    },
+    {
+        name: 'Bolt (2008)',
+        des: 'An animation adventure movie about the canine star of a fictional Sci-Fi action show that believes his powers are real and embarks on a cross-country trek to save his co-star from a threat',
+        image: 'images/slider 12.jpg'
+    },
+    {
+        name: 'SpongeBob SquarePants Series',
+        des: 'A yellow sea sponge named SpongeBob SquarePants, who enjoys being a cook at Krusty Krab, lives in the Pacific Ocean. He embarks on various adventures with his friends at Bikini Bottom',
+        image: 'images/slider 13.jpg'
+    },
+    {
+        name: 'Joker (2019)',
+        des: 'Arthur Fleck, a party clown, leads an impoverished life with his ailing mother. However, when society shuns him and brands him as a freak, he decides to embrace the life of crime and chaos.',
+        image: 'images/slider 14.jpg'
+    },
+    {
+        name: 'The Nightmare (2015 American film)',
+        des: 'The Nightmare is a 2015 American documentary film  directed by Rodney Ascher. The film had its world premiere on January 26, 2015 at the 2015 Sundance Film Festival  and focuses on the topic of sleep paralysis  Ascher chose his subject because it had happened to him in the pas',
+        image: 'images/slider 15.jpg'
+    },
+    {
+        name: 'The Lion King (1994)',
+        des: 'As a cub, Simba is forced to leave the Pride Lands after his father Mufasa is murdered by his wicked uncle, Scar. Years later, he returns as a young lion to reclaim his throne',
+        image: 'images/slider 16.jpg'
+    }
+]
